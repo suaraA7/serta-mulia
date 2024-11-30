b8f6e0fb6b65995fccbff55b76c75e9ed90025d7
SuaraAdiPratama <91652922+suaraA7@users.noreply.github.com>
2024-11-30 21:34:42 +0800

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -38,7 +38,7 @@ const InputError = require("../exceptions/InputError");
         status: "fail",
         message: response.message,
       });
-      newResponse.code(response.statusCode);
+      newResponse.code(response.output.statusCode);
       return newResponse;
     }
 
